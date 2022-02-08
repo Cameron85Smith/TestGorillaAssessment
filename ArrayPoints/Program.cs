b3d253482ee01e99e7c2203e082756b19b981c59
Cameron Smith <cameron85smith@gmail.com>
2022-02-08 14:49:40 +0200

    Initial Push

---
+++ ArrayPoints/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//int[] my_numbers = new int[] {1, 2, 3, 4, 5};
+//int[] my_numbers = new int[] {15, 25, 35};
+int[] my_numbers = new int[] {8, 8};
+
+
+
+int returnValue = 0;
+
+    foreach (var number in my_numbers)
+    {
+        if (number % 2 == 0 && number != 8)
+            returnValue++;
+
+        else if (number % 2 != 0 && number != 8)
+            returnValue += 3;
+
+        else if (number == 8)
+            returnValue += 5;
+}
+
+System.Console.WriteLine(returnValue);
